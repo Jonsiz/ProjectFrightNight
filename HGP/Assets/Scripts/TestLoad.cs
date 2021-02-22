4e04c2772e0d7d555775e0945e46eae1508e5d62
LittleFiery1 <47900497+LittleFiery1@users.noreply.github.com>
2021-02-21 19:06:35 -0600

    More testing the saver

--- HGP/Assets/Scripts/TestLoad.cs
+++ HGP/Assets/Scripts/TestLoad.cs
@@ -7,6 +7,15 @@ public class TestLoad : MonoBehaviour
     public GameObject checkpoint;
     void Update()
     {
+        //if (Input.GetKeyDown("h"))
+        //{
+        //    Debug.Log(GameObject.Find("SecurityGuard").GetComponent<EnemyAI>().testLoad);
+        //}
+        //if (Input.GetKeyDown("j"))
+        //{
+        //    GameObject.Find("SecurityGuard").GetComponent<EnemyAI>().testLoad = 100;
+        //}
+
         if (Input.GetKeyDown("l"))
         {
             GameObject.Find("SaveAndLoadObject").GetComponent<SaveAndLoadManager>().LoadingdaGame();
